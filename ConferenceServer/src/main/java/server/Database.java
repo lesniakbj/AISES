7fc59ff26678b33a9358ac2ff7dd78d20f713974
Brendan Lesniak <lesni019@umn.edu>
2016-07-30 17:46:49 -0500

    Added congfig files for the DB. Next step, some logging framework

--- ConferenceServer/src/main/java/server/Database.java
+++ ConferenceServer/src/main/java/server/Database.java
@@ -1,28 +1,46 @@
 package server;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Created by Brendan on 7/25/2016.
  */
 public class Database {
+    private static Properties properties = new Properties();
+
     // TODO: MOVE THIS TO THE POM OR RESOURCE FILES
     private static final String DB_DRIVER_NAME = "org.postgresql.Driver";
     private static final String CONNECTION_TYPE = "jdbc:postgresql";
-    private static final String HOST = "localhost";
-    private static final int PORT = 6670;
-    private static final String DB_NAME = "*********";
-    private static final String DB_PASS = "**********";
+    private static String HOST = "localhost";
+    private static int PORT = 6670;
+    private static String DB_NAME = "AISES";
+    private static String DB_PASS = "**********";
 
     private static Database instance = null;
     private Connection connection;
 
     protected Database() {
         try {
+            InputStream input = Database.class.getClassLoader().getResourceAsStream("db.properties");
+            properties.load(input);
+            HOST = properties.getProperty("host");
+            PORT = Integer.parseInt(properties.getProperty("port"));
+            DB_NAME = properties.getProperty("db_name");
+            DB_PASS = properties.getProperty("db_password");
+
             Class.forName(DB_DRIVER_NAME);
             connection = DriverManager.getConnection(CONNECTION_TYPE + "://" + HOST + ":" + PORT + "/" + DB_NAME, "postgres", DB_PASS);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
