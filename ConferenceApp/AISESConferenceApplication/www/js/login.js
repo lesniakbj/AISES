bece11884750a8df1cc6c68d0ea24059bd2b1176
Brendan Lesniak <lesni019@umn.edu>
2016-07-31 20:46:30 -0500

    Added basic facebook login integration. Tested working on Android

--- ConferenceApp/AISESConferenceApplication/www/js/login.js
+++ ConferenceApp/AISESConferenceApplication/www/js/login.js
@@ -13,34 +13,21 @@ AISES.LoginController = {
         
         // Attach any events here...
         $('#login-submit').on('click', AISES.LoginController.handleLogin);
-        $('#login-create').on('click', AISES.LoginController.handleCreateLogin);
     },
     
     handleLogin: function() {
-        // First pass normalization
-        var us = $('#login-username').val().trim();
-        var pas = $('#login-password').val().trim();
-        
-        if(AISES.Utils.isEmpty(us) || AISES.Utils.isEmpty(pas)) {
-            alert('Username and Password cannot be empty! Please try again.');
-            return;
-        }
-        
-        // Is this correct?
-        facebookConnectPlugin.login(["public_profile", "email"], AISES.LoginController.loginSuccess, 
-                                                                AISES.LoginController.loginFailure);
+        facebookConnectPlugin.login(["public_profile", "email"],
+                                    AISES.LoginController.loginSuccess, 
+                                    AISES.LoginController.loginFailure);
     },
     
     loginSuccess: function(userData) {
-        console.log(JSON.stringify(userData));
+        // Save any user data before we leave
         AISES.Routes.routeTo(AISES.Routes.HOME);
     },
     
     loginFailure: function(error) {
-        console.log("Error logging in! " + error);
-    },
-    
-    handleCreateLogin: function() {
+        alert("Unable to login, please try again!");
         return;
     }
 };
